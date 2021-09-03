db4543f10f7a8fde73bfabbb89157c752123dbdd
tessied <tessied@uchicago.edu>
2021-09-03 01:51:15 -0400

    Record correct guessed on the map

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,6 +7,26 @@ screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+indicator = turtle.Turtle()
+indicator.hideturtle()
+indicator.pu()
+
+correct_guesses = []
+
+data = pandas.read_csv("50_states.csv")
+states_list = data.state.to_list()
+
+while len(correct_guesses) < 50:
+    answer = screen.textinput(title=f"{len(correct_guesses)}/50 States Correct", prompt="Name a State: ").title()
+
+    if answer in states_list:
+        correct_guesses.append(answer)
+        x_loc = int(data[data.state == answer].x)
+        y_loc = int(data[data.state == answer].y)
+        indicator.goto(x_loc, y_loc)
+        indicator.write(answer, align="center")
+
 
 screen.exitonclick()
 
